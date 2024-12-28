b1736f920980f0b77caed974a360074a359c1c37
astroartics <joshisanjanana114@gmail.com>
2024-12-28 15:17:51 +0530

    Update

--- main_page.py
+++ main_page.py
@@ -10,8 +10,10 @@ footer {visibility: hidden;}
 .stToolbarActions {visibility: hidden;}
 .st-emotion-cache-1p1m4ay {visibility: hidden;}
 .e3i9eg82 {visibility: hidden;}
-._container_gzau3_1 _viewerBadge_nim44_23 {visibility: hidden;}
+._container_gzau3_1 {visibility: hidden;}
+._viewerBadge_nim44_23 {visibility: hidden;}
 </style>
 
 """
+
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
