867c38f41f6344c4c7aae8143be4e08b033659f6
astroartics <joshisanjanana114@gmail.com>
2024-12-28 14:21:11 +0530

    Update

--- main_page.py
+++ main_page.py
@@ -2,3 +2,12 @@ import streamlit as st
 
 st.markdown("# Main page 🎈")
 st.sidebar.markdown("# Main page 🎈")
+
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
